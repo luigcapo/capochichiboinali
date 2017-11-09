207f558dbfd0a730254c77ee65dbc5f9cf1db886
luigcapo <luigicapochichi1996@gmail.com>
2017-11-09 18:58:51 +0100

    restint

---
+++ src/shared/engine/HandleFondationCommand.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include "HandleFondationCommand.h"
+
+namespace engine{
+
+    void HandleFondationCommand::TestFonda() {
+
+    }
+
+    void HandleFondationCommand::execute(state::State& state) {
+
+    }
+
+    CommandTypeId HandleFondationCommand::getTypeId() const {
+        return CommandTypeId::HANDLE_FONDA;
+    }
+
+}

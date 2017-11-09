882797a7796bde95c6ee3dfdc23813ac72388419
boinali <yassert.boinali@ensea.fr>
2017-11-10 00:09:04 +0100

    maj fichiers sources

--- src/shared/engine.h
+++ src/shared/engine.h
@@ -6,8 +6,6 @@
 #include "engine/Command.h"
 #include "engine/LoadCommand.h"
 #include "engine/OrientationCommand.h"
-#include "engine/HandleCollisionsCommand.h"
-#include "engine/HandleFondationCommand.h"
 #include "engine/AttaqueCommand.h"
 #include "engine/FondationCommand.h"
 #include "engine/Engine.h"

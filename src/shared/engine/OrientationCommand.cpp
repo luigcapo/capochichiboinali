de4573b8754700bca76e5f3d2841ca5de31a58b0
luigcapo <luigicapochichi1996@gmail.com>
2017-11-09 15:58:15 +0100

    maj3

--- src/shared/engine/OrientationCommand.cpp
+++ src/shared/engine/OrientationCommand.cpp
@@ -8,19 +8,47 @@
 namespace engine{
 
     OrientationCommand::OrientationCommand(int c, state::Direction d) {
-
+        this->character=c;
+        this->orientation=d;
     }
 
     void OrientationCommand::execute(state::State& state) {
+        state::ElementTab listt=state.getChars();
+        for(unsigned int i=0;i<listt.getHeight();i++){
+            for(unsigned 
+                    int j=0;i<listt.getWidth();i++){
+                state::Element* e=listt.get(i,j);
+                if(e->getTypeId()!= character){}
+                else{  
+                    if(e->isStatic()==true){}
+                    else{
+                        if (orientation==state::Direction::NORTH){e->setY(e->getY()+1);}
+                        else if(orientation==state::Direction::NORTH_EAST){e->setX(e->getX()+1);e->setY(e->getY()+1);}
+                        else if(orientation==state::Direction::NORTH_WEST){e->setX(e->getX()-1);e->setY(e->getY()+1);}
+                        else if(orientation==state::Direction::SOUTH){e->setY(e->getY()-1);}
+                        else if(orientation==state::Direction::SOUTH_EAST){e->setX(e->getX()+1);e->setY(e->getY()-1);}
+                        else if(orientation==state::Direction::SOUTH_WEST){e->setX(e->getX()-1);e->setY(e->getY()-1);}
+                        else if(orientation==state::Direction::EAST){e->setX(e->getX()+1);}
+                        else if(orientation==state::Direction::WEST){e->setX(e->getX()-1);}
+                        else{}    
+                    }
+                }
+                
+            }    
+        }
 
     }
 
     int OrientationCommand::getCharacter() const {
-
+        return character;
     }
 
     void OrientationCommand::setCharacter(int character) {
+        this->character=character;
+    }
 
+    state::Direction OrientationCommand::getOrientation() const {
+        return orientation;
     }
 
     

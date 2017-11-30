500195a81484292f0fff6883f57b93208b331c5b
boinali <yassert.boinali@ensea.fr>
2017-11-30 23:30:11 +0100

    creation test heuristic_ai

---
+++ src/shared/tests/TestHeuristicAI.h
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TestHeuristicAI.h
+ * Author: mbcity
+ *
+ * Created on 30 novembre 2017, 22:12
+ */
+
+#ifndef TESTHEURISTICAI_H
+#define TESTHEURISTICAI_H
+#include "state.h"
+#include "ai.h"
+#include "engine.h"
+
+void testHeuristicAI();
+
+
+#endif /* TESTHEURISTICAI_H */
+

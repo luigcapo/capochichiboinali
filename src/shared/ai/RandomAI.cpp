31a665dbe8e459e5182f202c9f929c1d6a5def67
luigcapo <luigicapochichi1996@gmail.com>
2017-12-04 14:51:03 +0100

    FIN TEST RANDOM_AI

--- src/shared/ai/RandomAI.cpp
+++ src/shared/ai/RandomAI.cpp
@@ -17,25 +17,18 @@ namespace ai{
 
     void RandomAI::run(engine::Engine& engine) {
         std::vector<Command*> list;
+        auto c=clock();
+        int randomSeed=c;
         for(size_t i=1;i<engine.getState().getChars().getWidth()-1;i++){
             for(size_t j=1;j<engine.getState().getChars().getHeight()-1;j++){
                 if(engine.getState().getChars().get(i,j)){
                     listCommands(list, engine.getState().getChars().get(i,j));
-                    engine.addCommand(list[randgen() % list.size()]);
+                    engine.addCommand(list[randomSeed % list.size()]);
                     engine.update();
-                    
-                    //std::cout<<"euh"<<std::endl;
-                    for(size_t h=1;h<list.size();h++){
-                    std::cout<<"euh"<<std::endl;
-                        delete(list[h]);
-                    }
                     list.clear();
                 }
-                
             }
-        }
-        
+        }   
     }
-
 }
 

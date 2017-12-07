9cc49bb4cf2c3d0109038ec19b0c27adef655734
boinali <yassert.boinali@ensea.fr>
2017-12-08 00:00:46 +0100

    Heuristic ai

--- src/shared/ai/HeuristicAI.cpp
+++ src/shared/ai/HeuristicAI.cpp
@@ -17,6 +17,41 @@ namespace ai{
         this->batJoueur2Map.init(state);
         this->unitJoueur1Map.init(state);
         this->unitJoueur2Map.init(state);
+        
+        /*
+         * 
+         * 1. On ajoute des puits aux endroits qui
+         * nous intéressent sur les différents maps
+         * 
+         */
+        for(std::size_t i=0; i<state.getGrid().getWidth(); i++){
+            for(std::size_t j=0; j<state.getGrid().getHeight(); j++){
+                if(state.getGrid().get(i, j)){
+                    if(state.getGrid().get(i, j)->getTypeId() == state::TypeId::BATIMENT){
+                        if(state.getGrid().get(i, j)->getJ() == 0){
+                            this->batJoueur1Map.addSink(Point(i, j, 0));
+                        }
+                        else if(state.getGrid().get(i, j)->getJ() == 1){
+                            this->batJoueur2Map.addSink(Point(i, j, 0));
+                        }
+                    }
+                }
+                if(state.getChars().get(i, j)){
+                    // on teste si joueur 1
+                    if(state.getChars().get(i, j)->getJ() == 0){
+                        this->unitJoueur1Map.addSink(Point(i, j, 0));
+                    }
+                    // on teste si joueur 2
+                    if(state.getChars().get(i, j)->getJ() == 1){
+                        this->unitJoueur2Map.addSink(Point(i, j, 0));
+                    }
+                }
+            }
+        }
+        this->batJoueur1Map.update(state);
+        this->batJoueur2Map.update(state);
+        this->unitJoueur1Map.update(state);
+        this->unitJoueur2Map.update(state);
     }
     
     const PathMap& HeuristicAI::getBatJoueur1Map() const {
@@ -35,52 +70,10 @@ namespace ai{
         return this->unitJoueur2Map;
     }
 
-    void HeuristicAI::run(engine::Engine& engine) {
-        
+    void HeuristicAI::run(engine::Engine& engine) { 
         /*
          * 
-         * 1. On ajoute des puits aux endroits qui
-         * nous intéressent sur les différents maps
-         * 
-         */
-        // unitJoueur1Map et unitJoueur2Map
-        for(std::size_t i=0; i<engine.getState().getChars().getWidth(); i++){
-            for(std::size_t j=0; j<engine.getState().getChars().getHeight(); j++){
-                if(engine.getState().getChars().get(i, j)){
-                    // on teste si joueur 1
-                    if(engine.getState().getChars().get(i, j)->getJ() == 0){
-                        this->unitJoueur1Map.addSink(Point(i, j, 0));
-                    }
-                    // on teste si joueur 2
-                    if(engine.getState().getChars().get(i, j)->getJ() == 1){
-                        this->unitJoueur2Map.addSink(Point(i, j, 0));
-                    }
-                }
-            }
-        }
-        this->unitJoueur1Map.update(engine.getState());
-        this->unitJoueur2Map.update(engine.getState());
-        // batJoueur1Map et batJoueur2Map
-        for(std::size_t i=0; i<engine.getState().getGrid().getWidth(); i++){
-            for(std::size_t j=0; j<engine.getState().getGrid().getHeight(); j++){
-                if(engine.getState().getGrid().get(i, j)){
-                    if(engine.getState().getGrid().get(i, j)->getTypeId() == state::TypeId::BATIMENT){
-                        if(engine.getState().getGrid().get(i, j)->getJ() == 0){
-                            this->batJoueur1Map.addSink(Point(i, j, 0));
-                        }
-                        else if(engine.getState().getGrid().get(i, j)->getJ() == 1){
-                            this->batJoueur2Map.addSink(Point(i, j, 0));
-                        }
-                    }
-                }
-            }
-        }
-        this->batJoueur1Map.update(engine.getState());
-        this->batJoueur2Map.update(engine.getState());
-        
-        /*
-         * 
-         * 2. Partie 1 Heuristic_ai
+         * 2. Heuristic_ai
          * 
          * Les unités du joueur 2 vont se déplacer alétoirement si elles ne
          * sont pas attaquées sinon elles se défendent en attaquant à leur tour
@@ -109,22 +102,25 @@ namespace ai{
                                 break;
                         }*/
                     }
-                    
-                    
-                    
+
                     // on teste si joueur  1 (par défaut)
                     if(engine.getState().getChars().get(i, j)->getJ() == 0){
                         // pour se rapprocher des unités ennemies
-                        //Point pp1(i, j, 0);
-                        //Point pp2(i, j, 0);
-                        //if(unitJoueur1Map.getWeight(pp1) < unitJoueur1Map.getWeight(pp2)) {  
-                                
+                        Point pp1 = unitJoueur2Map.getWeightMin(i,j);
+                        //MoveCommmand(pp)
+                        if(engine.getState().getChars().get(pp1.getX(), pp1.getY())){
+                            if(engine.getState().getChars().get(pp1.getX(), pp1.getY())->getJ() == 1){
+                                engine.addCommand(new engine::AttaqueCommand(i, j, pp1.getX(), pp1.getY()));
+                            }
+                            else{engine.addCommand(new engine::MoveCommand(i, j, pp1.getX(), pp1.getY()));}
+                        }
                         
                     }
                 }
             }
         }
+        engine.update();
         
-    }   // Heuristic_ai
+    }// Heuristic_ai
     
 }
